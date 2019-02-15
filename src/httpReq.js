3dd783163151e1e0026e497009894c77156b3b2d
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-02-15 15:05:47 +0900

    [refactoring]

---
+++ src/httpReq.js
@@ -0,0 +1,16 @@
+const https = require('https');
+
+const req = https.request('https://github.com/9sako6/MESA', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+        console.log('No more data in response.');
+    });
+})
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+req.end();
