d08206871d002ad48dbd9552bfb3a575826dd524
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-05-07 16:10:43 +0900

    :recycle: refactoring

--- src/CSModel.js
+++ src/CSModel.js
@@ -1,14 +1,14 @@
 class CSModel {
   constructor() {
     // init
-    this.username = "";
-    this.repo = "";
+    this.username = '';
+    this.repo = '';
     this.page = 1;
     this.commitsCount = null;
 
     //
     // responseJSON restores histories of response from GitHub API
-    // key is "username-repo-page"
+    // key is 'username-repo-page'
     //
     this.responseJSON = new Object();
   }
