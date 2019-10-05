81f8bc192d8f96fad9950bc8f2566b712c6dbbbe
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-10-05 23:59:50 +0900

    refactoring and add circleci

--- src/APIUtils.ts
+++ src/APIUtils.ts
@@ -1,47 +1,34 @@
-import $ from 'jquery';
-import { renderErrorMessage } from './helper';
+import axios from "axios";
+import { renderErrorMessage } from "./helper";
 /**
- * HTTP request to obtain total commit count
+ * request total commit count
  */
 export async function requestTotalCommitNum(user: string, repo: string) {
-  return new Promise((onFulfilled, onRejected) => {
-    $.ajax({
-      url: `https://secure-tundra-40881.herokuapp.com/count?user=${user}&repo=${repo}`
-    })
-      .then(response => {
-        onFulfilled(response);
-      })
-      .catch((error: any) => {
-        console.error(error);
-        if (error.responseJSON !== undefined) {
-          renderErrorMessage(error.status, error.responseJSON.message);
-        } else {
-          renderErrorMessage(error.status, "That's an error.");
-        }
-        onRejected(error);
-      });
-  });
+  try {
+    const { data } = await axios.get(
+      `https://secure-tundra-40881.herokuapp.com/count?user=${user}&repo=${repo}`
+    );
+    return data;
+  } catch (err) {
+    renderErrorMessage(err.status, err.message);
+    throw new Error(err.message);
+  }
 }
-
-export function requestCommitsHistory(
+/**
+ * request commit history via GitHub API
+ */
+export async function requestCommitHistory(
   user: string,
   repo: string,
   page: number
 ) {
-  return new Promise((onFulfilled, onRejected) => {
-    $.ajax({
-      url: `https://api.github.com/repos/${user}/${repo}/commits?page=${page}&per_page=100`
-    }).then(
-      // success
-      response => {
-        onFulfilled(response);
-      },
-      // fail
-      error => {
-        console.error(error);
-        renderErrorMessage(error.status, error.responseJSON.message);
-        onRejected(error);
-      }
+  try {
+    const { data } = await axios.get(
+      `https://api.github.com/repos/${user}/${repo}/commits?page=${page}&per_page=100`
     );
-  });
+    return data;
+  } catch (err) {
+    renderErrorMessage(err.status, err.message);
+    throw new Error(err.message);
+  }
 }
