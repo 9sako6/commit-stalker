44106847c1b65a28e688287164ae982236fe0a3c
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-10-06 23:33:53 +0900

    add footer & rate limit card

---
+++ src/components/RequestLeftCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./request-left-card.scss";
+
+export const REQUEST_LEFT_CARD_ID = "request-left-card";
+export default function RequestLeftCard() {
+  return (
+    <div className="request-left-card">
+      <a href="https://developer.github.com/v3/#rate-limiting">
+        The GitHub API's rate limit allows for up to 60 requests per hour
+      </a>
+    </div>
+  );
+}
--- src/helper.tsx
+++ src/helper.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { PAGE_INFO_CLASS } from "./components/PageInfo";
 
 export function renderErrorMessage(status: number, message: string) {
+  document.querySelectorAll(PAGE_INFO_CLASS).forEach(elem => {
+    ReactDOM.render(<></>, elem);
+  });
   ReactDOM.render(
     <div>
       <h1>{status}</h1>
