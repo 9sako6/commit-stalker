e8176d3911c02dc53466c0579650382cfcdb858a
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-11-16 01:04:31 +0900

    update header design & delete footer

--- src/components/RequestLeftCard.tsx
+++ src/components/RequestLeftCard.tsx
@@ -5,7 +5,7 @@ export const REQUEST_LEFT_CARD_ID = "request-left-card";
 export default function RequestLeftCard() {
   return (
     <div className="request-left-card">
-      <a href="https://developer.github.com/v3/#rate-limiting">
+      <a href="https://developer.github.com/v3/#rate-limiting" target="_blank">
         The GitHub API's rate limit allows for up to 60 requests per hour
       </a>
     </div>
