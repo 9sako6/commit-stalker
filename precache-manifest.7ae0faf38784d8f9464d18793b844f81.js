85bf71a9188d006a3061d15c26941d1fde46c83e
9sako6 <9sako6@users.noreply.github.com>
2020-02-26 09:55:04 +0000

    Automated deployment: Wed Feb 26 09:55:04 UTC 2020 50c63afc494ff6aeccf5156dad16e845695ae5b6

---
+++ precache-manifest.7ae0faf38784d8f9464d18793b844f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8120ee851507acb81c5776cac5fc1a6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3275abfac06defa8987",
+    "url": "/static/css/main.6a9696c1.chunk.css"
+  },
+  {
+    "revision": "7167f869c0c3221d7388",
+    "url": "/static/js/2.121e2733.chunk.js"
+  },
+  {
+    "revision": "af7eb1c5eea68aaa9737138632f77641",
+    "url": "/static/js/2.121e2733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3275abfac06defa8987",
+    "url": "/static/js/main.a8ab69db.chunk.js"
+  },
+  {
+    "revision": "214c25f503c4848830a4",
+    "url": "/static/js/runtime-main.f3456661.js"
+  }
+]);
