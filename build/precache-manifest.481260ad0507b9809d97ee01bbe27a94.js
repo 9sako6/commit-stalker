92d66ef4c0227af20399c276a125f298e1878d50
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-02-26 19:05:46 +0900

    add deploy command

---
+++ build/precache-manifest.481260ad0507b9809d97ee01bbe27a94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988cbbf77a0e76370b235ffc0d505455",
+    "url": "/commit-stalker/index.html"
+  },
+  {
+    "revision": "df4fc9670342333e6612",
+    "url": "/commit-stalker/static/css/main.6a9696c1.chunk.css"
+  },
+  {
+    "revision": "bd0445a6f8e78c91737a",
+    "url": "/commit-stalker/static/js/2.77bccc0d.chunk.js"
+  },
+  {
+    "revision": "af7eb1c5eea68aaa9737138632f77641",
+    "url": "/commit-stalker/static/js/2.77bccc0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4fc9670342333e6612",
+    "url": "/commit-stalker/static/js/main.d3dc0666.chunk.js"
+  },
+  {
+    "revision": "3ffa296d2727c256f058",
+    "url": "/commit-stalker/static/js/runtime-main.af50e04c.js"
+  }
+]);
