9963b59f5e13cb91ae5b3b2badedb67da75fc486
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-05-04 23:32:50 +0900

    test: react-testing-library

--- src/typings/react-useanimations.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'react-useanimations' {
-  import React from 'react';
-  export interface UseAnimationsProps {
-    style?: object;
-    animationKey: string;
-    size: number;
-  }
-  export default class UseAnimations extends React.Component<UseAnimationsProps> {}
-}
