9c0909c5d89f984aa3414812eaca55c7982bbdab
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-08-28 23:31:33 +0900

    Show commit messages in Markdown
    I was able to print the entire list using `react-list`.
    However, the algorithm for calculating row height could use some improvement.

--- src/components/commitHistory/index.tsx
+++ src/components/commitHistory/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { CommitRow, CommitInfo } from 'src/components/commitRow';
 import { GitHubAPIResponse } from 'src/typings/github-api';
 import { maxWidth } from 'src/utils/common';
+import ReactList from 'react-list';
 
 type CommitHistoryProps = {
   jsonList: GitHubAPIResponse[];
   user: string;
   repo: string;
 };
+
+const dateHeight = 24;
+
 export const CommitHistory = ({ jsonList, user, repo }: CommitHistoryProps) => {
   if (jsonList === undefined) {
     return <></>;
@@ -17,11 +21,12 @@ export const CommitHistory = ({ jsonList, user, repo }: CommitHistoryProps) => {
     root: {
       marginLeft: 'auto',
       marginRight: 'auto',
-      marginBottom: '2rem',
+      marginBottom: '1rem',
       padding: '1rem',
       maxWidth,
     },
-    title: {
+    date: {
+      height: dateHeight,
       color: '#586069',
       fontSize: '14px',
       margin: '1.5em 0 0.8em 0',
@@ -40,23 +45,39 @@ export const CommitHistory = ({ jsonList, user, repo }: CommitHistoryProps) => {
     commitUrl: json.html_url ?? '#',
   }));
 
+  const rowHeight = Array(jsonList.length).fill(200);
+  const calcHeight = (lineCounts: number, isIncludeDate: boolean) => {
+    const marginSize = 24 * Math.min(lineCounts, 1);
+    const othersSize = 70;
+    const lineSize = 14;
+    return marginSize + othersSize + (lineCounts - 1) * lineSize + (isIncludeDate ? dateHeight : 0);
+  };
+  const rows = commitJsonList.map((json: CommitInfo, index: number) => {
+    const sha = json.sha;
+    const dateList = String(new Date(jsonList[index].commit.committer.date)).split(' ').slice(1, 4);
+    const date = `${dateList[0]} ${dateList[1]}, ${dateList[2]}`;
+    rowHeight[index] = calcHeight(json.commitMessage.split('\n').length, date !== prevDate);
+    if (date !== prevDate) {
+      prevDate = date;
+      return (
+        <div key={sha}>
+          <div style={styles.date}>{date}</div>
+          <CommitRow json={json} rowHeight={rowHeight[index]} user={user} repo={repo} />
+        </div>
+      );
+    } else {
+      return <CommitRow key={sha} rowHeight={rowHeight[index]} json={json} user={user} repo={repo} />;
+    }
+  });
+
   return (
     <div style={styles.root}>
-      {commitJsonList.map((json: CommitInfo, i) => {
-        const dateList = String(new Date(jsonList[i].commit.committer.date)).split(' ').slice(1, 4);
-        const date = `${dateList[0]} ${dateList[1]}, ${dateList[2]}`;
-        if (date !== prevDate) {
-          prevDate = date;
-          return (
-            <React.Fragment key={i}>
-              <div style={styles.title}>{date}</div>
-              <CommitRow json={json} user={user} repo={repo} />
-            </React.Fragment>
-          );
-        } else {
-          return <CommitRow key={i} json={json} user={user} repo={repo} />;
-        }
-      })}
+      <ReactList
+        itemRenderer={(index: number) => rows[index]}
+        length={commitJsonList.length}
+        type="variable"
+        itemSizeGetter={(index) => rowHeight[index]}
+      />
     </div>
   );
 };
