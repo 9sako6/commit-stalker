ddec10fd389f7fa056cd286813dc44415c6f7e74
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-08-29 00:18:14 +0900

    Updates

---
+++ precache-manifest.f156bcc002fd797c37da9d6978ada245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3b53731604a3bdd0000c394e2f5ed3",
+    "url": "/commit-stalker/index.html"
+  },
+  {
+    "revision": "d6d8a704d42da6f7c1bc",
+    "url": "/commit-stalker/static/css/main.8c5b6cf1.chunk.css"
+  },
+  {
+    "revision": "7636d172eb4d604fb3ab",
+    "url": "/commit-stalker/static/js/2.d035348f.chunk.js"
+  },
+  {
+    "revision": "029cacb393da85609b7166ec787b372d",
+    "url": "/commit-stalker/static/js/2.d035348f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d8a704d42da6f7c1bc",
+    "url": "/commit-stalker/static/js/main.1e9115ff.chunk.js"
+  },
+  {
+    "revision": "e08317aa0c63def255be",
+    "url": "/commit-stalker/static/js/runtime-main.1749daf1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/commit-stalker/precache-manifest.e2b6d1dd7ea686a4df6abbad747ba0fa.js"
+  "/commit-stalker/precache-manifest.f156bcc002fd797c37da9d6978ada245.js"
 );
 
 self.addEventListener('message', (event) => {
