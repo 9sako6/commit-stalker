837cd56ae4ec4d5a2ecdb0e32447b3e24a49e2ad
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-08-29 00:17:11 +0900

    Delete outdated test

--- src/tests/commitHistory.test.js
+++ src/tests/commitHistory.test.js
@@ -6,11 +6,6 @@ import jsonList from './fixtures/rails_rails_20200504214243.json';
 
 const setup = (props) => render(<CommitHistory {...props} />);
 
-test('It should render CommitRow(li element) when it gets correct props', () => {
-  const { container } = setup({ jsonList });
-  expect(container.querySelectorAll('li.commit-list-item')).toHaveLength(jsonList.length);
-});
-
 test('It should render nothing when jsonList props is undefined', () => {
   const { container } = setup({ jsonList: undefined });
   expect(container.querySelectorAll('li.commit-list-item')).toHaveLength(0);
