b4c42b2c3b890c405249719eeaed08e57e1db812
9sako6 <31821663+9sako6@users.noreply.github.com>
2021-07-19 20:15:35 +0900

    Use standard instead of prettier

--- src/tests/readme.test.js
+++ src/tests/readme.test.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Readme } from 'src/components/readme';
+/* eslint-env jest */
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Readme } from 'src/components/readme'
 
 test('readme should has two p tags and two h1 tags', () => {
-  const { container, getByText } = render(<Readme />);
-  const titleElement = getByText('Commit Stalker');
-  expect(titleElement).toBeInTheDocument();
+  const { container, getByText } = render(<Readme />)
+  const titleElement = getByText('Commit Stalker')
+  expect(titleElement).toBeInTheDocument()
 
-  expect(container.querySelectorAll('p')).toHaveLength(2);
-  expect(container.querySelectorAll('h1')).toHaveLength(2);
-});
+  expect(container.querySelectorAll('p')).toHaveLength(2)
+  expect(container.querySelectorAll('h1')).toHaveLength(2)
+})
