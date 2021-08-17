b7b9e67fe492e68b2730f7e684fb92e89343e92e
9sako6 <31821663+9sako6@users.noreply.github.com>
2021-08-18 00:01:56 +0900

    Delete unused component

--- src/components/commitHistory/calcRowHeight.ts
+++
@@ -1,8 +0,0 @@
-const dateHeight = 24;
-
-export const calcRowHeight = (lineCounts: number, isIncludeDate: boolean) => {
-  const marginSize = 24 * Math.min(lineCounts, 1);
-  const othersSize = 70;
-  const lineSize = 14;
-  return marginSize + othersSize + (lineCounts - 1) * lineSize + (isIncludeDate ? dateHeight : 0);
-};
