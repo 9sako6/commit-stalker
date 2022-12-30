f8e12f3ef194dea3c06817de35090952039c18dd
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-12-30 16:39:04 +0900

    Add page form

--- commit-stalker/src/components/organisms/search-form.tsx
+++ commit-stalker/src/components/organisms/search-form.tsx
@@ -2,21 +2,26 @@ import { useState } from "react"
 import { FormEventHandler } from "react"
 
 export const SearchForm = () => {
-  const [value, setValue] = useState('')
+  const [query, setQuery] = useState('')
+  const [page, setPage] = useState(1)
 
-  const handleChange: FormEventHandler<HTMLInputElement> = (e) => {
-    setValue(e.currentTarget.value)
+  const handleQueryChange: FormEventHandler<HTMLInputElement> = (e) => {
+    setQuery(e.currentTarget.value)
+  }
+  const handlePageChange: FormEventHandler<HTMLInputElement> = (e) => {
+    setPage(Number(e.currentTarget.value))
   }
 
   return <form className="w-2/4 flex py-12">
     <input
       autoFocus
-      name="repository" onChange={handleChange} value={value} placeholder='owner/repository'
+      name="repository" onChange={handleQueryChange} value={query} placeholder='owner/repository'
       className="appearance-none border-b border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-800" />
     <input
       autoFocus
-      name="page" value={1} placeholder='1'
-      className="ml-4 text-center appearance-none border-b border-gray-200 w-20 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-800" />
+      type='number'
+      name="page" onChange={handlePageChange} value={page} placeholder='1' min={1}
+      className="ml-4 text-center appearance-none border-b border-gray-200 w-20 py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-800" />
     <button className="ml-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-3 border border-gray-400 shadow rounded">Search</button>
   </form>
 }
