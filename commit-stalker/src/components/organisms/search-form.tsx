fc221bdc5123fbde739368086812dfe2ac34d65f
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-12-31 05:03:42 +0900

    Update form design

--- commit-stalker/src/components/organisms/search-form.tsx
+++ commit-stalker/src/components/organisms/search-form.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react"
 import { useState } from "react"
 import { FormEventHandler } from "react"
+import SearchIcon from '@mui/icons-material/Search'
+import TagIcon from '@mui/icons-material/Tag';
 
 type Props = {
   owner: string;
@@ -25,15 +27,21 @@ export const SearchForm = ({ owner, repository, page }: Props) => {
   }
 
   return <form className="text-sm md:text-base w-2/4 flex py-4">
-    <input
-      autoFocus
-      name="repository" onChange={handleQueryChange} value={query} placeholder='owner/repository'
-      className="appearance-none border-b border-gray-200 md:w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-800" />
-    <input
-      autoFocus
-      type='number'
-      name="page" onChange={handlePageChange} value={pageFromInput} placeholder='1' min={1}
-      className="ml-4 text-center appearance-none border-b border-gray-200 w-20 py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-800" />
-    <button className="ml-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-1 px-3 border border-gray-400 shadow rounded">Search</button>
+    <div className="flex items-center">
+      <SearchIcon className="text-gray-700 text-base" />
+      <input
+        autoFocus
+        name="repository" onChange={handleQueryChange} value={query} placeholder='owner/repository'
+        className="bg-transparent appearance-none md:w-full py-2 px-1 text-gray-700 leading-tight focus:outline-none focus:text-blue-600" />
+    </div>
+    <div className="flex items-center mr-4">
+      <TagIcon className="text-gray-700 text-base" />
+      <input
+        autoFocus
+        type='number'
+        name="page" onChange={handlePageChange} value={pageFromInput} placeholder='1' min={1}
+        className="bg-transparent appearance-none w-20 py-2 px-1 text-gray-700 leading-tight focus:outline-none focus:text-blue-600" />
+    </div>
+    <button className="bg-transparent hover:bg-gray-100 text-gray-800 text-sm font-semibold py-1 px-2 border border-gray-400 rounded">Search</button>
   </form>
 }
