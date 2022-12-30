89ecaa662d06113d04b6f46b9f26d93d1b239e3c
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-12-30 15:16:34 +0900

    Add libraries to parse markdown to HTML

---
+++ commit-stalker/src/lib/md-to-html.ts
@@ -0,0 +1,16 @@
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+
+export const mdToHtml = async (markdown: string) => {
+  return unified()
+    .use(remarkParse) // Parse markdown content to a syntax tree
+    .use(remarkRehype) // Turn markdown syntax tree to HTML syntax tree, ignoring embedded HTML
+    .use(rehypeStringify) // Serialize HTML syntax tree
+    .process(markdown)
+    .then((file) => String(file))
+    .catch((_error) => {
+      // nothing
+    })
+}
