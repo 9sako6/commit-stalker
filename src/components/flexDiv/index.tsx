e6375aa1b792850b03496db5ced2eee9fa66db17
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-12-31 23:38:32 +0900

    Replace files

--- src/components/flexDiv/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const FlexDiv: React.FC<{ style: object }> = ({ children, style }) => {
-  return <div style={{ ...style, display: 'flex' }}>{children}</div>;
-};
