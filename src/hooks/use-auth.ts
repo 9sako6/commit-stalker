c135326644d302368b62b8b5f84d2dd309ae59be
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-05-01 19:51:00 +0900

    Save token in session storage instead of local storage

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,22 +1,31 @@
-import { getAuth, signInWithPopup, GithubAuthProvider, connectAuthEmulator, signOut as fbSignOut } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GithubAuthProvider,
+  signOut as fbSignOut,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
 import { useAccessToken } from "./use-access-token";
 
 export const useAuth = () => {
-  const provider = new GithubAuthProvider()
-  const [_, setAccessToken, removeAccessToken] = useAccessToken()
-  const auth = getAuth()
-  const signIn = () => signInWithPopup(auth, provider)
-    .then(result => {
-      const credential = GithubAuthProvider.credentialFromResult(result)
-      const accessToken = credential?.accessToken
-      if (accessToken) {
-        setAccessToken(accessToken)
-      }
-    })
-  const signOut = () => {
-    removeAccessToken()
-    fbSignOut(auth)
-  }
+  const provider = new GithubAuthProvider();
+  const [_, setAccessToken, removeAccessToken] = useAccessToken();
+  const auth = getAuth();
+  setPersistence(auth, browserSessionPersistence);
 
-  return [signIn, signOut] as const
-}
+  const signIn = () =>
+    signInWithPopup(auth, provider).then((result) => {
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      const accessToken = credential?.accessToken;
+      if (accessToken) {
+        setAccessToken(accessToken);
+      }
+    });
+  const signOut = () => {
+    removeAccessToken();
+    fbSignOut(auth);
+  };
+
+  return [signIn, signOut] as const;
+};
